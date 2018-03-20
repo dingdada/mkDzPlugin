aa50c927e43b9242ac694268a5a3e6f41be87fd0
dinglingjuan <dinglingjuan@didichuxing.com>
2018-03-20 18:52:43 +0800

    middle

---
+++ components/Page/PageWithTable.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import {post} from 'common/util/request';
+import {GET__PAGENAME_} from 'common/constants/api';
+
+import CustomTable from 'common/components/CustomTable';
+
+const LIMIT = 40;
+
+export default class _PAGENAME_ extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            params: {
+                start: 0,
+                limit: LIMIT
+            },
+            current: 0,
+            list: [],
+            total: []
+        };
+    }
+
+    componentDidMount() {
+        this.reload();
+    }
+
+    reload(params) {
+        params = params || {};
+        const initialParams = this.state.params;
+        const mergedParams = {
+            ...initialParams,
+            ...params
+        };
+
+        const {start, limit} = mergedParams;
+
+        this.setState({
+            params: mergedParams,
+            current: start ? start / limit + 1 : 0
+        });
+
+        post(GET__PAGENAME_, mergedParams).then(res => {
+            if (res.retcode) {}
+            else {
+                this.setState({
+                    list: res.data.root,
+                    total: parseInt(res.data.totalProperty, 10)
+                });
+            }
+        });
+    }
+
+    render() {
+    }
+}
--- template/fe/src/App.js
+++ template/fe/src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+const { Header, Content, Sider } = Layout;
 
 export default class App extends React.Component {
     constructor() {
         super();
     }
     render() {
-        return 'hello world';
+        return (
+            <Layout>
+                <Header className="header">
+                    <div className="logo"></div>
+                    <Menu
+                        theme="dark"
+                        mode="horizontal"
+                        defaultSelectedKeys={['1']}
+                        style={{ lineHeight: '64px' }}
+                    >
+                        <Menu.Item key="1">nav 1</Menu.Item>
+                    </Menu>
+                </Header>
+                <Layout  style={{ padding: '0 24px 24px' }}>
+                    <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
+                        Content
+                    </Content>
+                </Layout>
+            </Layout>
+        );
     }
 }
